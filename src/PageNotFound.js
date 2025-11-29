937060e1eccd66b5643b26698cf859246a5c6c14
ouissal rms <khramswissal@gmail.com>
2025-11-29 19:52:17 +0100

    update

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import Not from './404.png'
 import './Page.scss'
+import { Navigate, useNavigate } from 'react-router-dom'
 export default function PageNotFound() {
+    const Navigate = useNavigate();
+    function GoTohome(){
+
+        Navigate("/Home")
+    }
   return (
     <div className='all'>
      <div className='image'>
@@ -11,7 +17,7 @@ export default function PageNotFound() {
      <div className='message'>
         <h1>Oops !</h1>
         <p>the page you are looking for might have been removed had its name changed or is temporarity unavailable .</p>
-        <input type='submit' value="GO TO HOMEPAGE" />
+        <input type='submit' value="GO TO HOMEPAGE" onClick={GoTohome} />
      </div>
     </div>
   )

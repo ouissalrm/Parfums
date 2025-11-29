39d30b7ad9e37df870433e1c59a0c6a62c77e353
ouissal rms <khramswissal@gmail.com>
2025-11-29 19:42:55 +0100

    update

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Not from './404.png'
+import './Page.scss'
 export default function PageNotFound() {
   return (
-    <div>
+    <div className='all'>
      <div className='image'>
         <img src={Not} alt='imagenotfounf' />
 
      </div>
      <div className='message'>
-        <h1>Oops</h1>
+        <h1>Oops !</h1>
         <p>the page you are looking for might have been removed had its name changed or is temporarity unavailable .</p>
         <input type='submit' value="GO TO HOMEPAGE" />
      </div>

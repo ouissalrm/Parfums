90955027990a3067ac325e6a8afdfe17574ce409
ouissal rms <khramswissal@gmail.com>
2025-11-30 17:01:33 +0100

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,33 +1,45 @@
-import React from 'react'
-import './App.css';
-
-import { Route, BrowserRouter as Router ,Link, Routes} from 'react-router-dom';
-
-
-
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
-    function myFunction(){
-        var element = document.body;
-   element.classList.toggle("dark-mode");
-    }
+  function myFunction() {
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
   return (
-    <div>
-       <nav className='navbar'>
-      <div className='logoo'>
+    <nav className="navbar">
+      <div className="logoo">
         <h2>Parfuméa</h2>
       </div>
-    
-    <div className='navbar1'>
-       <Link to="/Home" className='nav-link'>Home</Link>
-        <Link to="/Products" className='nav-link'>Products</Link>
-           <Link to="/About" className='nav-link'>About</Link>
-             
-                 <Link to="/Shop" className='nav-link'>Shop</Link>
-                    <Link to="/Login" className='nav-link'>Login</Link>
-    </div>
-    <button onClick={myFunction}>Dark mode</button>
+
+      <div className="navbar1">
+        <Link to="/Home" className="nav-link">
+          Home
+        </Link>
+        <Link to="/Products" className="nav-link">
+          Products
+        </Link>
+        <Link to="/About" className="nav-link">
+          About
+        </Link>
+        <Link to="/Shop" className="nav-link">
+          Shop
+        </Link>
+        <Link to="/Login" className="nav-link">
+          Login
+        </Link>
+      </div>
+
+      <div className="toggle-wrapper">
+        <input
+          type="checkbox"
+          id="darkmode-toggle"
+          onClick={myFunction}
+        />
+        <label className="toggle-label" htmlFor="darkmode-toggle" />
+      </div>
     </nav>
-    </div>
-  )
+  );
 }
